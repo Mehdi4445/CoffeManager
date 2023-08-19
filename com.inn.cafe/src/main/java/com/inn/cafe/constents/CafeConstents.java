b757e3417f3ddf0097f66d9f7a49ec5c0dfd592a
Mehdi Medimegh <79759194+Mehdi4445@users.noreply.github.com>
2023-08-19 09:09:33 +0100

    created user register API

---
+++ com.inn.cafe/src/main/java/com/inn/cafe/constents/CafeConstents.java
@@ -0,0 +1,6 @@
+package com.inn.cafe.constents;
+
+public class CafeConstents {
+    public static final String OOPS = "SOMTHING WRONG !";
+    public static final String INVDAT = "INVALID DATA !";
+}
